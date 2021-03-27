6239c21cc7d95d54df384abb3cc3bb5fd7758829
John Moore <jmoore@zedstar.org>
2021-03-27 14:06:04 +0000

    example word cloud on part of the data

--- Code/ner-spacy-example/main.py
+++ Code/ner-spacy-example/main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import spacy
 from functools import reduce
-
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 def join_strings(x, y):
     return str(x) + " " + str(y)
@@ -37,5 +38,16 @@ orgs = []
 
 nlp = spacy.load("en_core_web_sm")
 
+# remove the limit on rows to do the entire file
 chunk = read_chunk("../../Data/regulation_search_results_10000.txt.gz", rows=10000)
 process(chunk)
+
+# this takes a bit of time because it needs a string so better to look for
+# some other visualisations that can work on a list of strings
+
+text = reduce(join_strings, orgs)
+wordcloud = WordCloud(width=480, height=480, margin=0).generate(text)
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.margins(x=0, y=0)
+plt.show()
