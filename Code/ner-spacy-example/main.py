59c41325e3fe57828319b9b6d007daa2953a3e99
John Moore <jmoore@zedstar.org>
2021-03-27 13:09:53 +0000

    simple example to ORGs from data

---
+++ Code/ner-spacy-example/main.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import spacy
+from functools import reduce
+
+
+def join_strings(x, y):
+    return x + " " + y
+
+
+def extract_orgs(doc):
+    for entity in doc.ents:
+        if entity.label_ == "ORG":
+            orgs.append(entity.text)
+
+
+def process(chunk):
+    for df in chunk:
+        text = reduce(join_strings, df[1])
+        doc = nlp(text)
+        extract_orgs(doc)
+
+
+def read_chunk(fname, chunk=100, rows=10000):
+    df = pd.read_csv(
+        fname,
+        header=None,
+        skiprows=0,
+        chunksize=chunk,
+        nrows=rows,
+        compression="gzip",
+        sep="|",
+    )
+    return df
+
+
+orgs = []
+
+nlp = spacy.load("en_core_web_sm")
+
+chunk = read_chunk("../../Data/regulation_search_results_10000.txt.gz")
+process(chunk)
