d05a684e689ed32271576066f878077c37949dc6
mark-bell-tna <mark.bell@nationalarchives.gov.uk>
2021-04-20 13:39:10 +0100

    Further methods added

--- Code/techne_library_code.py
+++ Code/techne_library_code.py
@@ -30,8 +30,9 @@ def clean_string(string):
                 out_string += " "
     return out_string
 
-def read_topic_list(topic_file):
+def read_topic_list(file_name):
     topic_words = {}
+    topic_file = open(file_name, 'r')
     for row in topic_file:
         fields = row[:-1].split("|")
         topic_id = int(fields[0])
@@ -39,4 +40,23 @@ def read_topic_list(topic_file):
         topic_words[topic_id] = words
     topic_file.close()
     return topic_words
+
+def read_doc_topics(file_name):
+    topics_per_doc = {}
+    doc_topics = open(file_name, 'r')
+    for row in doc_topics:
+        fields = row[:-1].split("|")
+        file_name = fields[0]
+        topic_probs = [float(x) for x in fields[1:]]
+        topics_per_doc[file_name] = topic_probs
+    doc_topic_file.close()
+    return topics_per_doc
+
+def plot_doc_topics(doc_ids, doc_topic_lookup, topic_count):
+    fig, ax = pyplot.subplots(2,2)
+    fig.set_size_inches(8.5,5)
+    for i, file_number in enumerate(doc_ids):
+        topic_probs = doc_topic_lookup["file_" + str(file_number) + ".txt"]
+        ax[int(i/2), i % 2].bar(x = [str(x) for x in range(topic_count)], height = topic_probs)
+    return fig, ax
 
